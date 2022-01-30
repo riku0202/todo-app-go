db536d57a9a335db99e071292c58535d53b3f875
riku0202 <riku0202excalibur@gmail.com>
2022-01-30 23:30:59 +0900

    app修正

---
+++ app/app.go
@@ -0,0 +1,23 @@
+package app
+
+import (
+	"github.com/riku0202/todo-app-go/domain/model"
+)
+
+type Repository interface {
+	Create(todo *model.Todo) error
+	Delete(id *model.ID) error
+	UpdateIsFinished(id *model.ID) error
+	FindByUserID(id *model.UserId) (*model.Todo, error)
+}
+
+type App struct {
+	repo Repository
+}
+
+func NewApp(repo Repository) *App {
+	app := &App{}
+	app.repo = repo
+
+	return app
+}
---
+++ app/createTodo.go
@@ -0,0 +1,22 @@
+package app
+
+// import (
+// 	"fmt"
+
+// 	"github.com/riku0202/todo-app-go/domain/model"
+// )
+
+// func (a App) CreateTodo(userId string, title string, description string) error {
+
+// 	title, err := model.NewTitle("asdafssf")
+// 	if err != nil {
+// 		return fmt.Errorf("titleを作成できません:%v", err)
+// 	}
+
+// 	// t, err := model.NewTodo()
+// 	// if err != nil {
+// 	// 	return fmt.Errorf("値の検証に失敗しました:%v", err)
+// 	// }
+
+// 	return nil
+// }
